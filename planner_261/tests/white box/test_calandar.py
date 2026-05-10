a3c3be24214efa3d404a3de1b98027fa0c784a80
MKM2626 <mkmiller06@icloud.com>
2026-05-11 07:45:51 +0800

    added some test to calendar

--- planner_261/tests/white box/test_calandar.py
+++ planner_261/tests/white box/test_calandar.py
@@ -1,1 +1,72 @@
-# test commit
+import pytest
+from unittest.mock import MagicMock
+
+from logic.Calendar import Calendar
+from logic.ConflictException import ConflictsException
+
+@pytest.mark.parametrize(
+    "month, day, start, end, result",
+    [(5, 0, 9, 10, "Day does not exist"),
+     (12, 10, 9, 10, "Month does not exist"),
+     (5, 10, 23, 10, "Illegal hour"),
+     (5, 10, 9, 24, "Illegal hour"),
+     (5, 10, 15, 10, "Meeting starts before it ends")],
+    ids=["1", "2", "3", "4", "5"],
+)
+def test_check_times(month, day, start, end, result):
+    with pytest.raises(ConflictsException, match=result):
+        Calendar.check_times(month, day, start, end)
+
+
+
+
+def test_clear_schedule():
+    cal = Calendar()
+
+    meeting = MagicMock()
+
+    cal.occupied[5][10].append(meeting)
+
+    # verify meeting exist
+    assert len(cal.occupied[5][10]) == 1
+
+    # remove meeting
+    cal.clear_schedule(5, 10)
+
+    # assert
+    assert cal.occupied[5][10] == []
+
+# print agenda month / day
+def test_print_agenda_empty():
+    cal = Calendar()
+
+    result = cal.print_agenda(5, 10)
+
+    assert result == "No Meetings booked on this date.\n\n"
+
+def test_print_agenda():
+    cal = Calendar()
+
+    meeting = MagicMock()
+    meeting.__str__.return_value = "Meeting"
+
+    cal.occupied[5][10].append(meeting)
+
+    result = cal.print_agenda(5, 10)
+
+    assert "Agenda for 5/10 are as follows:" in result
+    assert "Meeting" in result
+
+# get meeting
+def test_get_meeting():
+    cal = Calendar()
+
+    meeting = MagicMock()
+
+    cal.occupied[5][10].append(meeting)
+
+    result = cal.get_meeting(5, 10, 0)
+
+    assert result == meeting
+
+
