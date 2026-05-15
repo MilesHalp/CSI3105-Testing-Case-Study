d88a756e00eae9249793c277985639eba144c9e9
MKM2626 <mkmiller06@icloud.com>
2026-05-15 15:38:51 +0800

    Added path names ot test rooms

--- planner_261/tests/white box/test_room.py
+++ planner_261/tests/white box/test_room.py
@@ -6,26 +6,28 @@ from logic.Room import Room
 
 
 # test invalid add meeting to raise conflict exception
-def test_invalid_add_meeting():
+def test_add_meeting_RP1():
     room = Room("J204")
 
-    fake_meeting = MagicMock()
+    # Create mock meeting
+    meeting = MagicMock()
 
     # mock calendar.add_meeting to raise exception
     room.calendar = MagicMock()
     room.calendar.add_meeting.side_effect = ConflictsException("Time conflict")
 
-    # verify exception
+    # verify exception and add meeting
     with pytest.raises(ConflictsException) as exc_info:
-        room.add_meeting(fake_meeting)
+        room.add_meeting(meeting)
 
+    # Catches exception and test it has correct exception
     assert "Conflict for room J204" in str(exc_info.value)
     assert "Time conflict" in str(exc_info.value)
-
-    room.calendar.add_meeting.assert_called_once_with(fake_meeting)
+    # Confirm add meeting is called with correct variables
+    room.calendar.add_meeting.assert_called_once_with(meeting)
 
 # Test print agenda return self.calendar.print_agenda(month, day)
-def test_day_print_agenda():
+def test_print_agenda_RP2():
     room = Room("J204")
 
     # Mock calendar.print_agenda to return value
@@ -38,25 +40,25 @@ def test_day_print_agenda():
     room.calendar.print_agenda.assert_called_once(5, 12)
 
 # Test full only path of init, add, get id, print agenda, is busy, get meeting, remove meeting
-def test_room():
+def test_room_RP3():
     room = Room("J204")
 
     # Replace calendar with mock
     room.calendar = MagicMock()
 
-    fake_meeting = MagicMock()
+    meeting = MagicMock()
 
     #  Return values
     room.calendar.print_agenda.return_value = "print agenda Output"
     room.calendar.is_busy.return_value = True
-    room.calendar.get_meeting.return_value = fake_meeting
+    room.calendar.get_meeting.return_value = meeting
 
     # get_id
     assert room.get_id() == "J204"
 
     # add_meeting
-    room.add_meeting(fake_meeting)
-    room.calendar.add_meeting.assert_called_once_with(fake_meeting)
+    room.add_meeting(meeting)
+    room.calendar.add_meeting.assert_called_once_with(meeting)
 
     # print_agenda
     agenda = room.print_agenda(5)
@@ -70,7 +72,7 @@ def test_room():
 
     # get_meeting
     meeting = room.get_meeting(5, 10, 0)
-    assert meeting == fake_meeting
+    assert meeting == meeting
     room.calendar.print_agenda.assert_called_once_with(5, 10, 0)
 
     # remove_meeting
