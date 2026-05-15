bf7d86a2451c997cb81bfce75f082e36afa48528
luke-wb-code <lwishartbrown@gmail.com>
2026-05-15 15:07:52 +0800

    add_meeting

--- planner_261/tests/white box/test_cal_Luke.py
+++ planner_261/tests/white box/test_cal_Luke.py
@@ -3,14 +3,25 @@ from unittest.mock import MagicMock
 
 from logic.Calendar import Calendar
 from logic.ConflictException import ConflictsException
+from logic.Meeting import Meeting
+
 
 #4.1
-def test_add_meeting():
+def test_add_meeting(monkeypatch):
     #replace calendar with mock
     cal = Calendar()
+    meeting = MagicMock()
+
+    monkeypatch.setenv("m_month", 1)
+    monkeypatch.setenv("m_day", 2)
+    monkeypatch.setenv("m_start", 8)
+    monkeypatch.setenv("m_end", 12)
 
 
-    cal.meeting = MagicMock()
-    cal.meeting.
+    monkeypatch.setattr(cal, 'check_times', MagicMock(return_value=None))
+
+    monkeypatch.setenv("to_check.get_description", "Exists")
+    monkeypatch.setenv("to_check.get_start_time", "12")
+    monkeypatch.setenv("to_check.get_end_time", "8")
 
     cal.add_meeting(meeting)
